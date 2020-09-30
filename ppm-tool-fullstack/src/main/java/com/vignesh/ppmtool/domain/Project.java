48357fdf70f3c42bf23ab1fef4b7f1a12eefba60
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-09-30 08:40:01 -0700

    added project Repository

--- ppm-tool-fullstack/src/main/java/com/vignesh/ppmtool/domain/Project.java
+++ ppm-tool-fullstack/src/main/java/com/vignesh/ppmtool/domain/Project.java
@@ -1,4 +1,80 @@
 package com.vignesh.ppmtool.domain;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    private String projectName;
+    private  String projectIdentifier;
+    private String description;
+    private Date created_At;
+    private Date updated_At;
+
+    public Project() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreated_At() {
+        return created_At;
+    }
+
+    public void setCreated_At(Date created_At) {
+        this.created_At = created_At;
+    }
+
+    public Date getUpdated_At() {
+        return updated_At;
+    }
+
+    public void setUpdated_At(Date updated_At) {
+        this.updated_At = updated_At;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.created_At=new Date();
+
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updated_At=new Date();
+    }
 }
+
