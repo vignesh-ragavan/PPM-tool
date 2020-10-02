f0bb1f9b8a509603a160d42459ef2b57683aebc6
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-10-02 08:58:48 -0700

    intial react commit

---
+++ ppmtool-react-client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>welcome to ppm tool</h1>
+    </div>
+  );
+}
+
+export default App;
