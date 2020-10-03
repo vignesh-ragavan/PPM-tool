78757021a65638f8efdddd58aebe39681fe63c61
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-10-03 05:13:44 -0700

    add project component added

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router,Route} from "react-router-dom";
+import AddProject from './components/Project/AddProject';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Header></Header>
-      <Dashboard/>
+       <Route exact path="/dashboard" component={Dashboard}/>
+
+      <Route exact path="/addProject" component={AddProject}/>
     </div>
+    </Router>
   );
 }
 
