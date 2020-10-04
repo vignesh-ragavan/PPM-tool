9c234d8cfb7c08afff26d993ab15513d07dafbd8
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-10-04 09:38:44 -0700

    creating the state

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -5,9 +5,12 @@ import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter as Router,Route} from "react-router-dom";
 import AddProject from './components/Project/AddProject';
+import {Provider} from "react-redux";
+import store from "./store";
 
 function App() {
   return (
+    <Provider store={store}>
     <Router>
     <div className="App">
       <Header></Header>
@@ -16,6 +19,7 @@ function App() {
       <Route exact path="/addProject" component={AddProject}/>
     </div>
     </Router>
+    </Provider>
   );
 }
 
