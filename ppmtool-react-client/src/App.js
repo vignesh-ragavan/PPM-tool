1a9f276ddcbb5dd213343a482adc30fd8a191937
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-10-13 10:03:05 -0700

    added update project

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -7,6 +7,7 @@ import {BrowserRouter as Router,Route} from "react-router-dom";
 import AddProject from './components/Project/AddProject';
 import {Provider} from "react-redux";
 import store from "./store";
+import UpdateProject from './components/Project/UpdateProject';
 
 function App() {
   return (
@@ -15,7 +16,9 @@ function App() {
     <div className="App">
       <Header></Header>
        <Route exact path="/dashboard" component={Dashboard}/>
-
+       <Route exact
+                path="/updateProject/:id"
+                component={UpdateProject} />
       <Route exact path="/addProject" component={AddProject}/>
     </div>
     </Router>
